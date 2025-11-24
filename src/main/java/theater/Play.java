74bceea93b9b5c40616c7acf07d20888a1626f99
dma40 <dma99878@gmail.com>
2025-11-23 22:21:20 -0500

    fixed all checkstyle issues

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,24 @@
 package theater;
 
+/**
+* Class describing a play.
+ */
+
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
     }
 }
